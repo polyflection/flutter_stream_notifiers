b795a19c1a60c794c657729a76fc5317dac71a69
Naoto Miyamoto <naoto@polyflection.com>
2020-09-09 17:49:24 +0900

    Fix typo.

--- lib/flutter_stream_notifiers.dart
+++ lib/flutter_stream_notifiers.dart
@@ -1,7 +1,7 @@
 /// Provides a bunch of special notifiers derived from Flutter's
 /// ValueNotifier or ChangeNotifier, that additionally streams a value or
 /// notifier itself on a change notification.
-library flutter_stream_notifierss;
+library flutter_stream_notifiers;
 
 export 'src/change_stream_notifiers.dart';
 export 'src/value_stream_notifiers.dart';
