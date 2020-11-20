c02bb3648894f4131f3d3a6502f91621e590d58f
Naoto Miyamoto <naoto@polyflection.com>
2020-11-20 13:58:43 +0900

    Update the example to null-safety version.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingStreamController controller;
+  late final TextEditingStreamController controller;
   String text = '';
 
   @override
